bc2d389bea1e745f7eaf6b4d1af8660b73ee659b
Nat Budin <nbudin@patientslikeme.com>
2017-04-02 22:12:34 -0700

    Set default template during migration

--- db/migrate/20170403044048_add_email_template_to_ticket_types.rb
+++ db/migrate/20170403044048_add_email_template_to_ticket_types.rb
@@ -1,5 +1,21 @@
 class AddEmailTemplateToTicketTypes < ActiveRecord::Migration[5.1]
   def change
     add_column :ticket_types, :email_template, :text
+
+    reversible do |dir|
+      dir.up do
+        default_template = <<-HTML
+<h1>Purchase Confirmation</h1>
+
+<p>
+  Thanks for your ticket purchase!  You've bought a {{ ticket.ticket_type_name }} ticket for {{ ticket.event_name }}.
+</p>
+
+{{ receipt }}
+HTML
+
+        TicketType.update_all(email_template: default_template)
+      end
+    end
   end
 end
