5ef8e86923d2fa0db300d4534568644be55ab638
Nat Budin <nbudin@patientslikeme.com>
2017-08-17 10:26:00 -0700

    Remove all the extracted stuff

--- app/controllers/navigation_items_controller.rb
+++ app/controllers/navigation_items_controller.rb
@@ -6,37 +6,6 @@ class NavigationItemsController < ApplicationController
 
   layout 'admin'
 
-  # def index
-  # end
-
-  # def create
-  #   @navigation_item.save
-  #   respond_with @navigation_item, location: edit_event_navigation_item_path(@event, @navigation_item)
-  # end
-  #
-  # def destroy
-  #   @navigation_item.destroy
-  #   if @navigation_item.parent
-  #     redirect_to edit_event_navigation_item_path(@event, @navigation_item.parent)
-  #   else
-  #     redirect_to event_navigation_items_path(@event)
-  #   end
-  # end
-  #
-  # def update
-  #   @navigation_item.update(navigation_item_params)
-  #   respond_with @navigation_item, location: edit_event_navigation_item_path(@event, @navigation_item)
-  # end
-  #
-  # def sort
-  #   ids = params[:navigation_item_ids].map(&:to_i)
-  #   navigation_items = @event.navigation_items.find(ids)
-  #
-  #   navigation_items.each do |item|
-  #     item.update_column(:position, ids.index(item.id) + 1)
-  #   end
-  # end
-
   private
 
   def navigation_item_scope
