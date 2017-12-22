d643445218bc74adac32583fc3aec0052450bb59
Nat Budin <nbudin@patientslikeme.com>
2017-12-22 09:20:13 -0800

    Set up Capistrano environments to match the actual deployments for this app

--- config/deploy/annewil.rb
+++ config/deploy/annewil.rb
@@ -3,11 +3,12 @@
 # Defines a single server with a list of roles and multiple properties.
 # You can define all roles on a single server, or split them:
 
-# server "example.com", user: "deploy", roles: %w{app db web}, my_property: :my_value
+server "hempel.sugarpond.net", user: "deploy", roles: %w{app db web}
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
-
+set :deploy_to, "/var/www/eventlite-annewil"
+set :rails_env, 'production'
 
 # role-based syntax
 # ==================
--- config/deploy/occultopus.rb
+++ config/deploy/occultopus.rb
@@ -7,7 +7,8 @@ server "hempel.sugarpond.net", user: "deploy", roles: %w{app db web}
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
-
+set :deploy_to, "/var/www/eventlite"
+set :rails_env, 'production'
 
 # role-based syntax
 # ==================
