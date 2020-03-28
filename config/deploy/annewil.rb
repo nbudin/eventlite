cb15d4df8233db7dc25cf06d10cca1a962c12bf3
Nat Budin <natbudin@gmail.com>
2020-03-28 10:01:55 -0700

    Server move

--- config/deploy/annewil.rb
+++ config/deploy/annewil.rb
@@ -3,7 +3,7 @@
 # Defines a single server with a list of roles and multiple properties.
 # You can define all roles on a single server, or split them:
 
-server "hempel.sugarpond.net", user: "deploy", roles: %w{app db web}
+server "scanlon.sugarpond.net", user: "deploy", roles: %w{app db web}
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
